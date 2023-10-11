714da569e24a669474c41b2221bda03e4a08ff0f
olesti <ataly08@hotmail.com>
2023-10-11 15:53:25 +0300

    a

--- app/routes/($locale)._index.jsx
+++ app/routes/($locale)._index.jsx
@@ -2,11 +2,12 @@ import {defer} from '@shopify/remix-oxygen';
 import {Await, useLoaderData, Link} from '@remix-run/react';
 import {Suspense, useEffect, useRef} from 'react';
 import {Image, Money} from '@shopify/hydrogen';
-
+import {Scripts} from '@remix-run/react';
 import HomePageBanner from '~/components/HomePageBanner/Index';
 import FeaturedCollection from '~/components/Featured Collections/Index';
 import CollectionList from '~/components/Cllection List/Index';
 import BannerSlider from '../components/Header/Slider';
+import loadScript from 'load-script';
 
 export const meta = () => {
   return [{title: 'Hydrogen | Home'}];
@@ -34,7 +35,7 @@ export async function loader({context}) {
 export default function Homepage() {
   const data = useLoaderData();
   const MessengerRef = useRef(null);
-  useEffect(() => {
+  /*   useEffect(() => {
     MessengerRef.current.setAttribute('page_id', '111320718694277');
     MessengerRef.current.setAttribute('attribution', 'biz_inbox');
 
@@ -56,9 +57,45 @@ export default function Homepage() {
     })(document, 'script', 'facebook-jssdk');
     let chatbox = MessengerRef;
     console.log(chatbox);
-  }, []);
+  }, []); */
+  // fb((FB) => FB.CustomerChat.show(true));
   return (
     <div className="home">
+      <div id="fb-root" style={{display: 'block'}}></div>
+      <div
+        ref={MessengerRef}
+        id="fb-customer-chat"
+        className="fb-customerchat"
+      ></div>
+      <Scripts
+        id="messenger-tag"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `{var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "111320718694277");
+      chatbox.setAttribute("attribution", "biz_inbox");}`,
+        }}
+      ></Scripts>
+      <Scripts
+        id="messenger-sdk"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `{window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v17.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));}`,
+        }}
+      ></Scripts>
       <BannerSlider />
       <FeaturedCollection data={data.featuredCollection} />
       <CollectionList />
@@ -72,12 +109,6 @@ export default function Homepage() {
         id="fb-customer-chat"
         class="fb-customerchat"
       ></div> */}
-      <div id="fb-root" style={{display: 'block'}}></div>
-      <div
-        ref={MessengerRef}
-        id="fb-customer-chat"
-        className="fb-customerchat"
-      ></div>
     </div>
   );
 }
@@ -99,6 +130,35 @@ export default function Homepage() {
 //   );
 // }
 
+// let initialized = false;
+// let queue = [];
+
+// export function fb(callback) {
+//   if (initialized) {
+//     callback(window.FB);
+//   } else {
+//     queue.push(callback);
+//     if (!window.fbAsyncInit) {
+//       window.fbAsyncInit = () => {
+//         window.FB.init({
+//           appId: window.config.facebook.appId,
+//           autoLogAppEvents: true,
+//           status: true,
+//           cookie: true,
+//           xfbml: false,
+//           version: 'v3.2',
+//         });
+//         initialized = true;
+//         queue.forEach((cb) => cb(window.FB));
+//         queue = null;
+//       };
+//       const script = 'xfbml.customerchat.js';
+//       loadScript(`https://connect.facebook.net/en_US/sdk/${script}`, {
+//         async: true,
+//       });
+//     }
+//   }
+// }
 function RecommendedProducts({products}) {
   return (
     <div className="recommended-products">
