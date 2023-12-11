a3d8b83cf42607d465fa60df0123b8b9cf4e9f4e
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2023-12-11 16:31:36 +0300

    Update ($locale).collections.$handle.jsx

--- app/routes/($locale).collections.$handle.jsx
+++ app/routes/($locale).collections.$handle.jsx
@@ -1,4 +1,6 @@
 import {json, redirect} from '@shopify/remix-oxygen';
+import {Form} from '@remix-run/react';
+import {RangeSlider} from 'rsuite';
 import {useLoaderData, Link} from '@remix-run/react';
 import {
   Pagination,
@@ -16,7 +18,7 @@ export async function loader({request, params, context}) {
   const {handle} = params;
   const {storefront} = context;
   const paginationVariables = getPaginationVariables(request, {
-    pageBy: 24,
+    pageBy: 15,
   });
 
   if (!handle) {
@@ -37,49 +39,88 @@ export async function loader({request, params, context}) {
 
 export default function Collection() {
   const {collection} = useLoaderData();
+  console.log(collection);
 
   return (
     <div className="collection">
-      <h1>{collection.title}</h1>
-      <p className="collection-description">{collection.description}</p>
-      <div className="flex flex-wrap row items-center justify-items-center">
-        <div className="w-180  h-300 flex flex-wrap flex-col	">
-          <button>Filter</button>
-          <button>Filter</button>
-          <button>Filter</button>
-        </div>
-        <Pagination connection={collection.products}>
-          {({nodes, isLoading, PreviousLink, NextLink}) => (
-            <>
-              <PreviousLink>
-                {isLoading ? 'Loading...' : <span>↑ Load previous</span>}
-              </PreviousLink>
+      <h1 className="text-center">{collection.title}</h1>
+      <p className="flex justify-center mb-10">
+        <span className="lg:max-w-lg w-full text-center">
+          {collection.description}
+        </span>
+      </p>
+      <div className="h-[75px] w-full border-y mb-10 flex justify-center items-center">
+        <div className="flex w-[160px] justify-center items-center border-r h-full">
+          <div className="w-[35px] h-[35px] grid grid-cols-2 grid-rows-2 gap-[0.5px] mr-2">
+            {Array(4)
+              .fill('')
+              .map((idx) => (
+                <div key={idx} className="w-[15px] h-[15px] bg-slate-200"></div>
+              ))}
+          </div>
 
-              <ProductsGrid products={nodes} />
-              <br />
-              <NextLink>
-                {isLoading ? 'Loading...' : <span>Load more ↓</span>}
-              </NextLink>
-            </>
-          )}
-        </Pagination>
+          <div className="w-[35px] h-[35px] grid grid-cols-3 grid-rows-3 gap-[0.5px]">
+            {Array(9)
+              .fill('')
+              .map((idx) => (
+                <div key={idx} className="w-[10px] h-[10px] bg-slate-200"></div>
+              ))}
+          </div>
+        </div>
+        <div className="w-full"></div>
+        <div className="w-[160px] h-full border-l flex justify-center items-center">
+          Sort
+        </div>
       </div>
+      <Pagination connection={collection.products}>
+        {({nodes, isLoading, PreviousLink, NextLink}) => (
+          <>
+            <ProductsGrid products={nodes} />
+            <br />
+            <NextLink>
+              {isLoading ? 'Loading...' : <span>Load more ↓</span>}
+            </NextLink>
+          </>
+        )}
+      </Pagination>
     </div>
   );
 }
 
 function ProductsGrid({products}) {
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {products.map((product, index) => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            loading={index < 8 ? 'eager' : undefined}
-          />
-        );
-      })}
+    <div className="grid grid-cols-[auto_auto] px-5">
+      <div className="lg:min-w-[350px]">
+        <Form>
+          <div className="mb-4">
+            <p className="font-bold">PRİCE</p>
+            <RangeSlider defaultValue={[10, 50]} />
+          </div>
+          <div className="mb-4">
+            <p className="mb-2 font-bold">COLOR</p>
+            <p className="mb-1">ROSE</p>
+            <p className="mb-1">WHİTE</p>
+            <p>YELLOW</p>
+          </div>
+          <div>
+            <p className="mb-2 font-bold">METERİAL</p>
+            <p className="mb-1">10K GOLD</p>
+            <p className="mb-1">14K GOLD</p>
+            <p className="">18K GOLD</p>
+          </div>
+        </Form>
+      </div>
+      <div className="grid grid-cols-4 gap-10">
+        {products.map((product, index) => {
+          return (
+            <ProductItem
+              key={product.id}
+              product={product}
+              loading={index < 8 ? 'eager' : undefined}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
@@ -88,7 +129,7 @@ function ProductItem({product, loading}) {
   const variant = product.variants.nodes[0];
   const variantUrl = useVariantUrl(product.handle, variant.selectedOptions);
   return (
-    <Link className="w-full" key={product.id} prefetch="intent" to={variantUrl}>
+    <Link className="" key={product.id} prefetch="intent" to={variantUrl}>
       {product.featuredImage && (
         <Image
           alt={product.featuredImage.altText || product.title}
@@ -169,9 +210,10 @@ const COLLECTION_QUERY = `#graphql
         }
         pageInfo {
           hasPreviousPage
+          startCursor
+          hasNextPage
           hasNextPage
           endCursor
-          startCursor
         }
       }
     }
