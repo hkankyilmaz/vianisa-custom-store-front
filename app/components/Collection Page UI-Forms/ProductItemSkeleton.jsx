463193a4aa8d9bc47eae7b453a649ac936a300c2
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2024-01-11 16:58:14 +0300

    Create ProductItemSkeleton.jsx

---
+++ app/components/Collection Page UI-Forms/ProductItemSkeleton.jsx
@@ -0,0 +1,14 @@
+export default function ProductItemSkeleton() {
+  return (
+    <div className="">
+      <div className="font-montserratMd max-sm:text-[8px] text-[10px] tracking-[2px] text-[#2f2f2f] max-lg:mb-0 mb-3 ml-4">
+        Deneme
+      </div>
+
+      <div className="w-full relative overflow-hidden ">Deneme</div>
+
+      <h4 className="dynamic-margin-top mt-14 mb-1 font-montserratMd max-sm:text-[10px] text-[11px] tracking-[2.2px] text-[#2f2f2f] uppercase"></h4>
+      <div className="flex gap-[10px]">Deneme</div>
+    </div>
+  );
+}
